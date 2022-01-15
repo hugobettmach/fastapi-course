d505bf42a62eaab62029ac8584b8c08b72635683
Hugo Bettencourt Machado <hbett@equinor.com>
2022-01-15 15:37:12 +0100

    Add deploy step

--- app/main.py
+++ app/main.py
@@ -23,4 +23,4 @@ app.include_router(vote.router)
 
 @app.get("/")
 def hello_world():
-    return {"message": "Hello World!"}
+    return {"message": "Hello World successfully deployed from CI/CD pipeline!"}
